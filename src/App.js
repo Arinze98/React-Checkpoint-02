28c4d72e165c6ffc143f0d477abad11c2ea48252
onyelonu ochonma arinzechukwu <ochonmaarinze98@gmail.com>
2024-06-13 14:22:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import PlayersList from './PlayersList';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5">
+      <h1>FIFA Player Cards</h1>
+      <PlayersList />
+    </Container>
   );
-}
+};
 
 export default App;
